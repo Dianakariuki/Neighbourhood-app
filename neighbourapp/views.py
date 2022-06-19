3a310c10e8f83c9a515168dd8cdd694b94372f92
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-19 10:06:39 +0300

    This commit creates the function index

--- neighbourapp/views.py
+++ neighbourapp/views.py
@@ -7,3 +7,16 @@ import neighbour
 from neighbour.models import Posts, Profile , Business ,Neighbourhood
 from .forms import PostForm, ProfileForm , NeighbourhoodForm , BusinessForm
 from django.contrib import messages
+
+
+@login_required(login_url="/accounts/login/")
+def index(request):
+
+  current_user = request.user
+  other_neighbours = Profile.objects.filter(neighbourhood_id = request.user.profile.neighbourhood.id).all()
+  businesses = Business.objects.filter(neighbourhood_id = request.user.profile.neighbourhood.id).all()
+  posts = Posts.objects.filter(neighbourhood_id= request.user.profile.neighbourhood.id).all().order_by('-id')
+ 
+
+  return render (request , 'neighbour/index.html',{"message":message , "form":upload_neighbourhood ,"myneighbours":other_neighbours, "businesses":businesses ,"posts":posts})
+
