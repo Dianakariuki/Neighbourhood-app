e453468420a2f9f2b06bd74bb2edbfc0d3399d4f
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-19 10:29:08 +0300

    This commit sets up the settings.py

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'neighbourhood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neighbourhood',
+        'USER': 'moringa',
+    'PASSWORD':'access',
     }
 }
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
