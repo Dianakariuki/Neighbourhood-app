844d024907c34f8aca0382dc071a8fa5bfbe0fa6
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-19 09:51:46 +0300

    This commit made imports  in the  models.py

--- neighbourapp/models.py
+++ neighbourapp/models.py
@@ -1,3 +1,6 @@
+from distutils.command.upload import upload
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.forms import ImageField
