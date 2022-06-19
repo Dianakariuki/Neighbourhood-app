2fb8044c37ca0086378b0abccb28a18be6a5db36
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-19 09:58:19 +0300

    This commit imports modules on viewss

--- neighbourapp/views.py
+++ neighbourapp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
+from email import message
+from locale import currency
+from django.shortcuts import render , redirect
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+import neighbour
+from neighbour.models import Posts, Profile , Business ,Neighbourhood
+from .forms import PostForm, ProfileForm , NeighbourhoodForm , BusinessForm
+from django.contrib import messages
