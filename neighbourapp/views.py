4d1c5fd3570842a89bfad16ef2ce5905e8f1e2c9
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-19 10:07:40 +0300

    This commit  creates the function profile

--- neighbourapp/views.py
+++ neighbourapp/views.py
@@ -20,3 +20,31 @@ def index(request):
 
   return render (request , 'neighbour/index.html',{"message":message , "form":upload_neighbourhood ,"myneighbours":other_neighbours, "businesses":businesses ,"posts":posts})
 
+@transaction.atomic
+@login_required(login_url ='/accounts/login')
+def profile(request):
+  current_user = request.user
+  user_profile = Profile.objects.filter(id = current_user.id)
+
+
+
+  if request.method == 'POST':
+    create_profile = ProfileForm(request.POST ,request.FILES ,  instance = request.user.profile )
+
+    if create_profile.is_valid():
+      user_profile = create_profile.save(commit = False)
+      user_profile.save()
+
+      messages.success(request,('Your profile was successfully updated!'))
+      return redirect('profile')
+    
+    else:
+      messages.error(request,"please correct the error below")
+  
+  else:
+     create_profile= ProfileForm(instance= request.user.profile)
+
+    
+
+
+  return render (request,'neighbour/profile.html', {"form" : create_profile })
