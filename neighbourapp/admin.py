d70526719440c18b01c18687aec7d9150d08d9aa
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-19 10:20:13 +0300

    This commit creates admin

--- neighbourapp/admin.py
+++ neighbourapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Neighbourhood, Profile ,Business
 
 # Register your models here.
+admin.site.register(Business)
+admin.site.register(Profile)
+admin.site.register(Neighbourhood)
