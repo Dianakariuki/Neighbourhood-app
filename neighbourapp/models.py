faaf7ebdfaabdc2659f4d90d1ed7125b4f80e4ce
Diana Kariuki <dianakariuki842@gmail.com>
2022-06-19 09:55:13 +0300

    This commit creates a model class for profile

--- neighbourapp/models.py
+++ neighbourapp/models.py
@@ -35,3 +35,30 @@ class Neighbourhood(models.Model):
     Neighbourhood.objects.filter(id=self.id)
 
 
+
+class Profile(models.Model):
+  """
+  A model that contains user's info for the profile
+  """
+  name = models.CharField(max_length = 30 , null = False)
+  user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile',  blank =True)
+  bio = models.TextField(max_length=500 , blank =True)
+  photo=models.ImageField(upload_to="profile/" , blank=True)
+  profile_email=models.EmailField(max_length = 100, blank=True)
+  location = models.CharField(max_length=30 , blank=True)
+  neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE, blank =True, null=True)
+
+  def __str__(self):
+    return f'{self.user.username}'
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+     if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+     instance.profile.save()
+
+
+
